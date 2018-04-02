cbcb9718b778111c1cc9dc1732a21838c6947f6e
micahbernath <bernath.13@osu.edu>
2018-04-02 19:49:34 -0400

    Added team component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRouterModule } from './router.module';
+import { TeamsComponent } from './teams/teams.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
