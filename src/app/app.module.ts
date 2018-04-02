fd4d98437cfdae93a1baf4c49f497d87b070a3f1
mcreel31 <creel.17@osu.edu>
2018-04-02 19:49:26 -0400

    fixed routing and added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRouterModule } from './router.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 // add routes here
 const routes: Routes = [
   // sample below
-  //{path: 'books', component: BooksComponent }
+  {path: 'dashboard', component: DashboardComponent }
 ]
 
 @NgModule({
