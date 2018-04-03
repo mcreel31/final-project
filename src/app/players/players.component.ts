3672d6366f1fc71303ad8a98035b66783f30179e
micahbernath <bernath.13@osu.edu>
2018-04-03 14:35:00 -0400

    Edited player component

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
+  player = 'Michael Jordan';
 
   constructor() { }
 
