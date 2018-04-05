50545dadded48aef684e57191a78eb453dc7be02
mcreel31 <creel.17@osu.edu>
2018-04-05 12:58:23 -0400

    import nba library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NBA } from 'nba';
 
 
 import { AppComponent } from './app.component';
