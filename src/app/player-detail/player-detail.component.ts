872534c272f22bbbd9ebf40410fb36c829e50050
micahbernath <bernath.13@osu.edu>
2018-04-10 16:48:04 -0400

    Added player general info to dashboard

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PlayerInfo } from '../players/player';
 import { PlayersService } from '../players/players.service'
+import { ArrayType } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-player-detail',
@@ -11,6 +12,11 @@ export class PlayerDetailComponent implements OnInit {
   //players = [{"name": "John Wall"}, {"name": "Kevin Durant"}, {"name": "Kris Dunn"}]
   AllData : PlayerInfo.RootObject
   SelectedPlayer : string = 'Pick a player!'
+  GeneralInfo : PlayerInfo.Playerstatsentry
+  JerseyNumber: string
+  Position: string
+  City: string
+  TeamName: string
 
   players : PlayerInfo.Player[]
   //@Input() players: PlayerInfo.Player[];
@@ -23,9 +29,20 @@ export class PlayerDetailComponent implements OnInit {
       this.AllData = data;
       this.players = this.AllData.cumulativeplayerstats.playerstatsentry.map(a => a.player)
       this.SelectedPlayer = this.players[0].FirstName + " " + this.players[0].LastName
+      this.GeneralInfo = this.AllData.cumulativeplayerstats.playerstatsentry
+      .find(i => i.player.FirstName === 
+        this.SelectedPlayer.split(" ", 2)[0] && i.player.LastName === this.SelectedPlayer.split(" ", 2)[1]);
     },
       error => console.log(error)
     );
+  }
+
+  event() {
+   this.GeneralInfo = this.AllData.cumulativeplayerstats.playerstatsentry
+   .find(i => i.player.FirstName === 
+     this.SelectedPlayer.split(" ", 2)[0] && i.player.LastName === this.SelectedPlayer.split(" ", 2)[1]);
+     this.JerseyNumber = this.GeneralInfo.player.JerseyNumber;
+     this.Position = this.GeneralInfo.player.Position;
   }
 
 }
