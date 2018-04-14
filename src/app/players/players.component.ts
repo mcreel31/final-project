2f5a159e086e3db3f7a89b0fcb05371761f5a4b2
micahbernath <bernath.13@osu.edu>
2018-04-14 13:24:02 -0400

    Added second player

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -11,9 +11,29 @@ import { PlayersService } from '../players/players.service';
 export class PlayersComponent implements OnInit {
   //var that api data gets put into
   AllData : PlayerInfo.RootObject
-  
-  players: PlayerInfo.Player[] = [];
-  selectedPlayer: PlayerInfo.Player;
+  players: PlayerInfo.Player[] = []
+  selectedPlayer: string = 'Pick a Player!'
+  statistics: PlayerInfo.Playerstatsentry
+  Position: string
+  GamesPlayed: number
+  Fg2ptatt: number
+  Fg2ptmade: number
+  Fg3ptatt: number
+  Fg3ptmade: number
+  Ftatt: number
+  Ftmade: number
+
+  selectedPlayer2: string = 'Pick a Player!'
+  statistics2: PlayerInfo.Playerstatsentry
+  Position2: string
+  GamesPlayed2: number
+  Fg2ptatt2: number
+  Fg2ptmade2: number
+  Fg3ptatt2: number
+  Fg3ptmade2: number
+  Ftatt2: number
+  Ftmade2: number
+
 
   constructor(private _playersService: PlayersService) { }
 
@@ -23,14 +43,46 @@ export class PlayersComponent implements OnInit {
         this.AllData = data;
         console.log(this.AllData)
         this.players = this.AllData.cumulativeplayerstats.playerstatsentry.map(a => a.player)
+        this.selectedPlayer = this.players[0].FirstName + " " + this.players[0].LastName
+        this.statistics = this.AllData.cumulativeplayerstats.playerstatsentry
+          .find(i => i.player.FirstName === 
+        this.selectedPlayer.split(" ", 2)[0] && i.player.LastName === this.selectedPlayer.split(" ", 2)[1]);
+        this.event();
+        
       },
         error => console.log(error)
       );
   }
 
 
-  onSelect(player: PlayerInfo.Player): void {
-    this.selectedPlayer = player;
+  event() {
+    console.log("after event")
+    this.statistics = this.AllData.cumulativeplayerstats.playerstatsentry
+          .find(i => i.player.FirstName === 
+        this.selectedPlayer.split(" ", 2)[0] && i.player.LastName === this.selectedPlayer.split(" ", 2)[1]);
+        this.GamesPlayed = Number(this.statistics.stats.GamesPlayed["#text"])
+        this.Fg2ptatt = Number(this.statistics.stats.Fg2PtAtt["#text"])
+        this.Fg2ptmade = Number(this.statistics.stats.Fg2PtMade["#text"])
+        this.Fg3ptatt = Number(this.statistics.stats.Fg3PtAtt["#text"])
+        this.Fg3ptmade = Number(this.statistics.stats.Fg3PtMade["#text"])
+        this.Ftatt = Number(this.statistics.stats.FtAtt["#text"])
+        this.Ftmade = Number(this.statistics.stats.FtMade["#text"])
   }
+
+
+ event2() {
+   console.log("after event")
+   this.statistics2 = this.AllData.cumulativeplayerstats.playerstatsentry
+         .find(i => i.player.FirstName === 
+       this.selectedPlayer2.split(" ", 2)[0] && i.player.LastName === this.selectedPlayer2.split(" ", 2)[1]);
+       this.GamesPlayed2 = Number(this.statistics.stats.GamesPlayed["#text"])
+       this.Fg2ptatt2 = Number(this.statistics.stats.Fg2PtAtt["#text"])
+       this.Fg2ptmade2 = Number(this.statistics.stats.Fg2PtMade["#text"])
+       this.Fg3ptatt2 = Number(this.statistics.stats.Fg3PtAtt["#text"])
+       this.Fg3ptmade2 = Number(this.statistics.stats.Fg3PtMade["#text"])
+       this.Ftatt2 = Number(this.statistics.stats.FtAtt["#text"])
+       this.Ftmade2 = Number(this.statistics.stats.FtMade["#text"])
+ }
+
 
 }
